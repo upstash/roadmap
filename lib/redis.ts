3f2f197a8cdb64194a7404cc12d1aaa042fd85ae
Adem ilter <ademilter@gmail.com>
2022-04-06 13:29:12 +0300

    refactor

--- lib/redis.ts
+++ lib/redis.ts
@@ -3,9 +3,6 @@ import { Redis } from '@upstash/redis'
 export const databaseName =
   process.env.NODE_ENV === 'development' ? 'roadmap-dev' : 'roadmap'
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN
-})
+const redis = Redis.fromEnv()
 
 export default redis
